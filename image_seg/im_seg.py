fa78b8a396e3d1385b0ece56be5aba1ee6b37b1f
lzuehsow <lydiazuehsow@gmail.com>
2016-10-21 09:47:02 -0400

    Tweaked

--- image_seg/im_seg.py
+++ image_seg/im_seg.py
@@ -1,14 +1,31 @@
 import numpy as np
 import cv2
 
-# Displays image 
 
-img = cv2.imread('frisk.jpg')
-cv2.imshow('image',img)
-k = cv2.waitKey(0) & 0xFF
-if k == 27:         # wait for ESC key to exit
-    cv2.destroyAllWindows()
+class PictureManager(object):
+    # Displays image 
+    def __init__(self):
+        self.img = cv2.imread('frisk.jpg')
 
+        # Define some colors
+        redColor = (0,0,255)
+        greenColor = (0,255,0)
+        blueColor = (255,0,0)
+        whiteColor = (255,255,255)
+        blackColor = (0,0,0)
+
+        # Set pygame fake desktop size
+        screenwidth = 600
+        screenheight = 450
+
+        greenLower = (29,86,6)
+        greenUpper = (64,255,255)
+
+    def imseg(self):
+        cv2.imshow('image',self.img)
+        k = cv2.waitKey(0) & 0xFF
+        if k == 27:         # wait for ESC key to exit
+            cv2.destroyAllWindows()
 
 # Chunk of old code from here on- working on modifying it
 
@@ -17,10 +34,8 @@ class WebCam(object):
         return: center coordinates"""
 
     def __init__(self, bufsize = 100, counter = 0):
-        self.camera = cv2.VideoCapture(0)
-        self.ap = argparse.ArgumentParser()
-        self.ap.add_argument("-v","--video",
-            help="path to the(optional) video file")
+        self.camera = cv2.imread('frisk.jpg')
+
         self.bufsize = bufsize
         self.ap.add_argument("-b", "--buffer", type=int, default = 100,
             help="max buffer size")
@@ -53,132 +68,8 @@ class WebCam(object):
         # Draw a dot to represent the wand's coordinates
         cv2.circle(webcam.frame, center, 5, redColor, -1)
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-# # ****************** INITIALIZING STUFF ****************** #
-
-#     # Initialize pygame
-#     pygame.init()
-
-#     # Define some colors
-#     redColor = pygame.Color(0,0,255)
-#     greenColor = pygame.Color(0,255,0)
-#     blueColor = pygame.Color(255,0,0)
-#     whiteColor = pygame.Color(255,255,255)
-#     blackColor = pygame.Color(0,0,0)
-
-#     # Set pygame fake desktop size
-#     screenwidth = 600
-#     screenheight = 450
-
-#     size = (screenwidth, screenheight)
-#     screen = pygame.display.set_mode(size)
-
-#     greenLower = (29,86,6)
-#     greenUpper = (64,255,255)
-
-#     check = 0
-#     frame = 0
-#     spell_frame = 0
-#     eventcount = 0
-#     center = 0
-
-#     webcam = WebCam()
-
-#     # running = False
-#     # running = True
-
-#     BUTTON = pygame.USEREVENT+3
-#     button_event = pygame.event.Event(BUTTON)
-
-#     # Makes sure only the events we want are on the event queue
-#     allowed_events = [QUIT,GRID,BUTTON]
-#     pygame.event.set_allowed(allowed_events)
-
-    
-# # ****************** RUNTIME LOOP ****************** #
-#     # This is the main loop of the program. 
-
-#     while True:
-
-#         if menu.gamerunning == True:
-#             break
-#         elif menu.tutorielrunning == True:
-#             break
-
-#         gotcenter = webcam.getcenter(greenLower, greenUpper)
-
-#         if gotcenter == None:
-#             master.selected = False
-#         else:
-#             center = gotcenter[0]
-#             radius = gotcenter[1]
-
-#             (x,y) = center
-
-#             pygame.draw.circle(screen,menu.cursorcolor,(600-x,y),3,0)
-
-#             if radius >= calradi + 15:
-#                 pygame.event.post(button_event)
-#                 menu.cursorcolor = redColor
-#             else:
-#                 menu.cursorcolor = blueColor
-
-#         if key == ord("q"):
-#             # Release the camera, close open windows
-#             webcam.camera.release()
-#             cv2.destroyAllWindows()
-#             master.close()
-#             pygame.quit()
-
-#         time.sleep(.001)
-
-#         # Find the center of any green objects' contours
-#         gotcenter = webcam.getcenter(greenLower, greenUpper)
-#         if gotcenter == None:
-#             webcam.update_webcam((300, 225))
-#         else:
-#             center = gotcenter[0]
-#             radius = gotcenter[1]
-#             webcam.update_webcam(center)
-#             if radius > 20:
-#                 # If the radius is above a certain size we count it
-#                 webcam.pts.append(center)
-#                 webcam.rad.append(radius)
-#                 webcam.counter = webcam.counter + 1
-#                 (x,y) = center
-
-#             master.process_events()
-
-#         # Update the frames of the webcam video
-#         webcam.frame = cv2.flip(webcam.frame, 1)
-#         cv2.imshow("Frame",webcam.frame)
-#         key = cv2.waitKey(1) & 0xFF
-#         frame = frame + 1
-#         time.sleep(.001)
-#         if key == ord("q"):
-#             # Release the camera, close open windows
-#             webcam.camera.release()
-#             cv2.destroyAllWindows()
-#             master.close()
-#             pygame.quit()
-#         if key == ord("c"):
-#             # Clear spell chain
-#             model.spell_clear()
-#         if key == ord("r"):
-#             # Reset game
-#             check = 0
-#             enemy.x = 25
-#             enemy.y = 100
-#             enemy.hp = 100
-#             player.hp = 500
-
-
-#             if menu.tutorielrunning == True:
-#                 directions = menu.font.render("Press q to quit", 40, blackColor)
-#                 screen.blit(directions, (300, 15))
-
-#                 directions = menu.font.render("Press c to start casting again", 40, blackColor)
-#                 screen.blit(directions, (300, 35))
-
-#             pygame.display.update()
+# ****************** INITIALIZING STUFF ****************** #
+    picture = PictureManager()
+    picture.imseg()
