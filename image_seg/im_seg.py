697363b8f45ef8348b061566ff31fcc904300def
lzuehsow <lydiazuehsow@gmail.com>
2016-10-21 10:30:48 -0400

    Recent

--- image_seg/im_seg.py
+++ image_seg/im_seg.py
@@ -6,14 +6,14 @@ import imutils
 class PictureManager(object):
     # Displays image 
     def __init__(self):
-        self.img = cv2.imread('frisk.jpg')
+        self.img = cv2.imread('sprinkles.jpg')
 
-        # Define some colors
-        redColor = (0,0,255)
-        greenColor = (0,255,0)
-        blueColor = (255,0,0)
-        whiteColor = (255,255,255)
-        blackColor = (0,0,0)
+        # # Define some colors
+        # redColor = (0,0,255)
+        # greenColor = (0,255,0)
+        # blueColor = (255,0,0)
+        # whiteColor = (255,255,255)
+        # blackColor = (0,0,0)
 
         # Set pygame fake desktop size
         screenwidth = 600
@@ -22,69 +22,48 @@ class PictureManager(object):
         self.greenLower = (29,86,6)
         self.greenUpper = (64,255,255)
 
+
     def getcenter(self, greenLower, greenUpper):
         
         # Resizes the frame, blurs the frame, converts to HSV color space
-        self.img = imutils.resize(self.img, width=600)
+        img = imutils.resize(self.img, width=600)
         blurred = cv2.GaussianBlur(self.img,(11,11),0)
-        hsv = cv2.cvtColor(self.frame,cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(self.img,cv2.COLOR_BGR2HSV)
 
         # Constructs a mask for "green" objects, performs dilations and erosions to remove erroneous parts of the mask
         mask = cv2.inRange(hsv, greenLower, greenUpper)
         mask = cv2.erode(mask,None,iterations=1)
 
 
-
         # Finds contours in the mask, initializes the current (x,y) center
         self.cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,
             cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-    def imseg(self):
-        cv2.imshow('image',self.img)
+        if len(self.cnts) > 0:
+            # Find the largest contour in the mask, use it to compute the minimum enclosing circle and centroid for that contour
+            c = max(self.cnts,key=cv2.contourArea)
+            M = cv2.moments(c)
+            (center,radius) = cv2.minEnclosingCircle(c)
+            Mlist= [M["m10"], M["m00"],M["m01"],M["m00"]]
+
+            if any(Mlist) == 0:
+                return None
+            else:
+                center = (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"]))
+                return [center,radius]
+
+
+    def imseg(self, pic):
+        cv2.imshow('image',picture.img)
+        cv2.circle(picture.img,center[0],int(center[1])
         k = cv2.waitKey(0) & 0xFF
         if k == 27:         # wait for ESC key to exit
             cv2.destroyAllWindows()
-
-# Chunk of old code from here on- working on modifying it
-
-class WebCam(object):
-    """Runs the webcam and identifies green objects.
-        return: center coordinates"""
-
-    def __init__(self, bufsize = 100, counter = 0):
-        self.camera = cv2.imread('frisk.jpg')
-
-        self.bufsize = bufsize
-        self.ap.add_argument("-b", "--buffer", type=int, default = 100,
-            help="max buffer size")
-        self.pts = deque(maxlen=bufsize)
-        self.rad = []
-        self.counter = counter
-
-        self.calpts = deque(maxlen=bufsize)
-        self.calrad = []
-        self.calcounter = counter
-
-    def getcenter(self, greenLower, greenUpper):
-        self.args = vars(self.ap.parse_args())
-        (self.grabbed, self.frame) = self.camera.read() # Grabs the current frame
-        
-        # Resizes the frame, blurs the frame, converts to HSV color space
-        self.frame = imutils.resize(self.frame, width=600)
-        blurred = cv2.GaussianBlur(self.frame,(11,11),0)
-        hsv = cv2.cvtColor(self.frame,cv2.COLOR_BGR2HSV)
-
-        # Constructs a mask for "green" objects, performs dilations and erosions to remove erroneous parts of the mask
-        mask = cv2.inRange(hsv, greenLower, greenUpper)
-        mask = cv2.erode(mask,None,iterations=1)
-
-        # Finds contours in the mask, initializes the current (x,y) center
-        self.cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)[-2]
 
 if __name__ == '__main__':
 
 # ****************** INITIALIZING STUFF ****************** #
     picture = PictureManager()
-    picture.getcenter(self.greenLower, self.greenUpper)
-    picture.imseg()
+    center = picture.getcenter(picture.greenLower, picture.greenUpper)
+    print center
+    picture.imseg(picture.img)
